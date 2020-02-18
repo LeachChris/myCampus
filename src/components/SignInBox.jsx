cf3be9e8480f5d32ee7fb3116d377e05ed40ae43
Tan-int <57535667+Tan-int@users.noreply.github.com>
2020-02-17 21:39:35 -0600

    Work on Login Page

--- src/components/SignInBox.jsx
+++ src/components/SignInBox.jsx
@@ -2,19 +2,18 @@ import React from "react";
 
 function SignInBox(){
     return(
-    <div>
         <div className = "SignInBackgroundContainer">
             <div className = "SignInBackground">
                 <h1>See What Other Students Are Saying About Your Campus</h1>
+                <img src="./logo.png" alt="Logo"></img>
                 <form action="/Login.jsx">
                     <input type="text" name="username" placeholder = "Username"/>
                     <input type="text" name="password" placeholder = "Password"/>
                     <button type = "submit" name = "submit">Login</button>
                 </form>
-
+                <p className = "AccountState">Don't have an account? <span>Sign Up</span></p>
             </div>
         </div>
-     </div>
     )
 }
 
