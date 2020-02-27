acea646940c7688c526fb21b4a2877d0cfb188b1
Chris Leach <cdleach5@gmail.com>
2020-02-27 12:30:17 -0600

    Tried more things

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
-const expres = require('express');
+const express = require('express');
 const cors = require('cors');
 
-require ('dotenv').congig();
+require ('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(experss.json());
+app.use(express.json());
 
 app.listen(port, () => {
-    console.log('Server is running on port: ${pott}');
+    // console.log('Server is running on port: ${port}');
 });
