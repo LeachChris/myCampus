12a8b9be69167959fff8f1d297882e3bf23e54e0
subashsubbu <subashshrestha455@gmail.com>
2020-03-02 14:13:45 -0600

    added signup navbar(not working though)

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from "react";
 import { HashRouter as Router, Route, Link, NavLink} from "react-router-dom";
+import Navigation from "./Navigation";
 import SignInBox from "./SignInBox";
 import SignUpBox from  "./SignUpBox";
 import "./SignUp.css";
@@ -10,6 +11,7 @@ class SignUp extends Component{
     return(
         <Router basename="/react-auth-ui/">
             <div className="signup">
+                <Navigation/>
                 <div className="PageSwitcher">
                     <NavLink to = "/LogIn" activeClassName="PageSwitcher_Item_Active" className="PageSwitcher_Item">LogIn</NavLink>
                     <NavLink exact to ="/"  activeClassName="PageSwitcher_Item_Active" className="PageSwitcher_Item">SignUp</NavLink>
